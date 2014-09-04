7e16d6eadf1b76263baad8ab094d8a4113d227a7
Fumistar <francis.duffeal@gmail.com>
2014-09-04 21:49:01 +0200

    avatar nullable

--- path/src/Acme/EsBattleBundle/Entity/User.php
+++ path/src/Acme/EsBattleBundle/Entity/User.php
@@ -27,7 +27,7 @@ class User extends BaseUser
     /**
      * @var string
      *
-     * @ORM\Column(name="avatar", type="string", length=255)
+     * @ORM\Column(name="avatar", type="string", length=255,nullable=true)
      */
     private  $avatar;
 
