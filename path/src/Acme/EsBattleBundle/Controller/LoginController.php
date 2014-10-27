43157ed71e5d237f6e311e459151c0e0e352612c
Fumistar <francis.duffeal@gmail.com>
2014-10-27 18:07:00 +0100

    login (cherry picked from commit 45eb452)

--- path/src/Acme/EsBattleBundle/Controller/LoginController.php
+++ path/src/Acme/EsBattleBundle/Controller/LoginController.php
@@ -4,7 +4,7 @@ namespace Acme\EsBattleBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-use Acme\EsBattleBundle\Entity\User;
+use Acme\EsBattleBundle\Entity\User as User;
 
 use Symfony\Component\HttpFoundation\Response;
 
@@ -20,6 +20,7 @@ class LoginController extends Controller
     {
 
 
+
         /*$user = $this->getDoctrine()
             ->getRepository('AcmeEsBattleBundle:User')
             ->findByLogin($login);*/
@@ -27,18 +28,33 @@ class LoginController extends Controller
         $user = $this->getDoctrine()
             ->getRepository('AcmeEsBattleBundle:User')
             ->findOneBy(
-                array('username' => $email)
+                array('email' => $email)
             );
 
-
-        $encoders = array(new XmlEncoder(), new JsonEncoder());
-        $normalizers = array(new GetSetMethodNormalizer());
-
-        $serializer = new Serializer($normalizers, $encoders);
-
-        $json = $serializer->serialize($user, 'json');
-
-        return new Response($json, 201, array('Access-Control-Allow-Origin' => 'http://localhost:8000', 'Content-Type' => 'application/json'));
+	    if($user->isPasswordOk($password)){
+		    $json = $user->serialize();
+		    return new Response($json, 201, array('Access-Control-Allow-Origin' => 'http://localhost:8000', 'Content-Type' => 'application/json'));
+	    } else {
+		    return new Response(null, 404, array('Access-Control-Allow-Origin' => 'http://localhost:8000', 'Content-Type' => 'application/json'));
+	    }
 
     }
+
+	public function registerAction($email,$password,$username)
+	{
+
+		$user = new User;
+
+		$user->setEmail($email);
+		$user->setUsername($username);
+		$user->setPassword($user->makePassword($password));
+
+
+		$em = $this->getDoctrine()->getManager();
+		$em->persist($user);
+		$em->flush();
+
+		$json = $user->serialize();
+		return new Response($json, 201, array('Access-Control-Allow-Origin' => 'http://localhost:8000', 'Content-Type' => 'application/json'));
+	}
 }
--- path/src/Acme/EsBattleBundle/Entity/User.php
+++ path/src/Acme/EsBattleBundle/Entity/User.php
@@ -3,7 +3,6 @@
 namespace Acme\EsBattleBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use FOS\UserBundle\Model\User as BaseUser;
 
 use Doctrine\Common\Collections\ArrayCollection;
 
@@ -13,7 +12,7 @@ use Doctrine\Common\Collections\ArrayCollection;
  * @ORM\Table(name="user")
  * @ORM\Entity
  */
-class User extends BaseUser
+class User
 {
     /**
      * @var integer
@@ -32,6 +31,36 @@ class User extends BaseUser
     private  $avatar;
 
 
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(name="username", type="string", length=255)
+	 */
+	private  $username;
+
+
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(name="email", type="string", length=255)
+	 */
+	private  $email;
+
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(name="password", type="string", length=255)
+	 */
+	private  $password;
+
+
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(name="salt", type="string", length=255)
+	 */
+	private  $salt;
+
     /**
      * @ORM\ManyToMany(targetEntity="Clan")
      * @ORM\JoinTable(name="users_clans",
@@ -42,10 +71,7 @@ class User extends BaseUser
     private $clans;
 
     public function __construct() {
-
-        parent::__construct();
-        // your own logic
-
+	    $this->salt = base_convert(sha1(uniqid(mt_rand(), true)), 16, 36);
         $this->clans = new ArrayCollection();
     }
 
@@ -145,4 +171,120 @@ class User extends BaseUser
     {
         return $this->groupe;
     }
+
+	/**
+	 * Serializes the user.
+	 *
+	 * The serialized data have to contain the fields used by the equals method and the username.
+	 *
+	 * @return string
+	 */
+	public function serialize()
+	{
+		return serialize(array(
+			'id' => $this->id,
+			'username' => $this->username
+
+		));
+	}
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     * @return User
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string 
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return User
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string 
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     * @return User
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string 
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set salt
+     *
+     * @param string $salt
+     * @return User
+     */
+    public function setSalt($salt)
+    {
+        $this->salt = $salt;
+
+        return $this;
+    }
+
+    /**
+     * Get salt
+     *
+     * @return string 
+     */
+    public function getSalt()
+    {
+        return $this->salt;
+    }
+
+	public function makePassword($password){
+		return base_convert(sha1($password.$this->salt), 16, 36);
+	}
+
+	public function isPasswordOk($password){
+		return $this->password === $this->makePassword($password);
+	}
 }
