59265e0c4ce5648b074715ae33b7d4d1132cc33a
Bill Clinton <francis.duffeal.external@watchever.com>
2015-06-03 18:05:50 +0200

    partenaire nouvelle version

--- path/src/Acme/EsBattleBundle/Controller/PartenaireController.php
+++ path/src/Acme/EsBattleBundle/Controller/PartenaireController.php
@@ -3,6 +3,9 @@
 namespace Acme\EsBattleBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+use Acme\EsBattleBundle\Entity\Partenaire;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
 
@@ -11,9 +14,15 @@ class PartenaireController extends Controller
 
     public function indexAction(){
 
-	    $collectionPartenaire = $this->getDoctrine()
-		    ->getRepository('AcmeEsBattleBundle:Partenaire')
-		    ->findAll();
+	    $em = $this->getDoctrine()->getManager();
+
+	    $query = $em->createQuery(
+		    'SELECT partenaire
+            FROM AcmeEsBattleBundle:Partenaire partenaire
+            JOIN partenaire.videos'
+	    );
+
+	    $collectionPartenaire = $query->getResult();
 
         $aResult = [];
         /**
@@ -25,8 +34,8 @@ class PartenaireController extends Controller
 
 	    $response = new JsonResponse();
 	    $response->setPublic();
-	    $response->setMaxAge(30);
-	    $response->setSharedMaxAge(30);
+	    $response->setMaxAge(300);
+	    $response->setSharedMaxAge(300);
 	    $response->setData($aResult);
 	    return $response;
     }
@@ -42,9 +51,169 @@ class PartenaireController extends Controller
 
 		$response = new JsonResponse();
 		$response->setPublic();
-		$response->setMaxAge(30);
-		$response->setSharedMaxAge(30);
+		$response->setMaxAge(300);
+		$response->setSharedMaxAge(300);
 		$response->setData($partenaire->_toArray());
+		return $response;
+	}
+
+	public function adminAction(Request $request){
+		if(!$request->getSession()->get('modo')){
+			$response = new Response();
+			$response->setStatusCode(401);
+			return $response;
+		}
+		/**
+		 * @var \Doctrine\Common\Collections\ArrayCollection $partenaireCollection
+		 */
+		$partenaireCollection = $this->getDoctrine()
+			->getRepository('AcmeEsBattleBundle:Partenaire')
+			->findAll();
+
+		return $this->render('AcmeEsBattleBundle:Partenaire:list.html.twig', array(
+			'partenaires' => $partenaireCollection
+		));
+	}
+
+	public function createAction(Request $request){
+
+		if(!$request->getSession()->get('modo')){
+			$response = new Response();
+			$response->setStatusCode(401);
+			return $response;
+		}
+
+		/**
+		 * @var \Acme\EsBattleBundle\Entity\Partenaire $partenaire
+		 */
+		$partenaire = new Partenaire();
+
+		$form = $this->createFormBuilder($partenaire)
+			->add('nom')
+			->add('description')
+			->add('youtube')
+			->add('twitch')
+			->add('facebook')
+			->add('twitter')
+			->add('logo','entity', array(
+				'empty_value' => 'Choisissez une image',
+				'required' => false,
+				'class' => 'AcmeEsBattleBundle:Document',
+				'property' => 'name',
+			))
+			->add('tuile','entity', array(
+				'empty_value' => 'Choisissez une image',
+				'required' => false,
+				'class' => 'AcmeEsBattleBundle:Document',
+				'property' => 'name',
+			))
+			->add('header','entity', array(
+				'empty_value' => 'Choisissez une image',
+				'required' => false,
+				'class' => 'AcmeEsBattleBundle:Document',
+				'property' => 'name',
+			))
+			->add('blocHomeImg','entity', array(
+				'empty_value' => 'Choisissez une image',
+				'required' => false,
+				'class' => 'AcmeEsBattleBundle:Document',
+				'property' => 'name',
+			))
+			->add('blocHomeLink','entity', array(
+				'empty_value' => 'Choisissez une image',
+				'required' => false,
+				'class' => 'AcmeEsBattleBundle:Document',
+				'property' => 'name',
+			))
+			->getForm();
+
+		$form->handleRequest($request);
+
+		if ($form->isValid()) {
+			$em = $this->getDoctrine()->getManager();
+			$em->persist($partenaire);
+			$em->flush();
+
+			$response = $this->forward('AcmeEsBattleBundle:Partenaire:admin');
+		} else {
+
+			$response = $this->render('AcmeEsBattleBundle:Partenaire:form.html.twig', array(
+				'form' => $form->createView()
+			));
+		}
+
+		return $response;
+	}
+
+	public function updateAction($id,Request $request){
+
+		if(!$request->getSession()->get('modo')){
+			$response = new Response();
+			$response->setStatusCode(401);
+			return $response;
+		}
+
+		/**
+		 * @var \Acme\EsBattleBundle\Entity\Partenaire $partenaire
+		 */
+		$partenaire = new Partenaire();
+
+		$form = $this->createFormBuilder($partenaire)
+			->add('nom')
+			->add('description')
+			->add('youtube')
+			->add('twitch')
+			->add('facebook')
+			->add('twitter')
+			->add('logo')
+			->add('tuile')
+			->add('header')
+			->add('blocHomeImg')
+			->add('blocHomeLink')
+			->getForm();
+
+		if($partenaire !== null){
+			$form->handleRequest($request);
+
+			if ($form->isValid()) {
+				$em = $this->getDoctrine()->getManager();
+				$em->flush();
+			}
+		}
+
+		$response = $this->render('AcmeEsBattleBundle:Partenaire:form.html.twig', array(
+			'partenaire'  => $partenaire,
+			'form' => $form->createView()
+		));
+
+		return $response;
+	}
+
+	public function deleteAction($id){
+		$session = new Session();
+
+		if(!$session->get('modo')){
+			$response = new Response();
+			$response->setStatusCode(401);
+			return $response;
+		}
+
+		/**
+		 * @var \Acme\EsBattleBundle\Entity\Planification $planification
+		 */
+		$planification = $this->getDoctrine()
+			->getRepository('AcmeEsBattleBundle:Planification')
+			->find($id);
+
+		if($planification !== null){
+			$em = $this->getDoctrine()->getManager();
+			$em->remove($planification);
+
+			$em->flush();
+		}
+
+		$response = $this->forward('AcmeEsBattleBundle:Planification:list');
+
 		return $response;
 	}
 }
