c8d4b932a6bce27bafae2053ea9fafdd50389f42
Fumistar <francis.duffeal@gmail.com>
2015-09-17 21:09:53 +0200

    planification, home, twitch, usergame, bungie

--- path/src/Acme/EsBattleBundle/Controller/HomeController.php
+++ path/src/Acme/EsBattleBundle/Controller/HomeController.php
@@ -12,6 +12,8 @@ use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Validator\Constraints\DateTime;
 use Doctrine\ORM\EntityRepository;
 
+use Acme\EsBattleBundle\Entity\Topic;
+
 use Doctrine\ORM\Tools\Pagination\Paginator;
 
 use Symfony\Component\HttpFoundation\Response;
@@ -23,6 +25,30 @@ class HomeController extends Controller
 
     public function indexAction()
     {
+
+        $arrayStatus = array(Topic::STATUS_NEWS);
+        $em = $this->getDoctrine()->getManager();
+
+        $query = $em->createQuery(
+            'SELECT topic, messages, user
+            FROM AcmeEsBattleBundle:Topic topic
+            JOIN topic.user user
+            JOIN topic.messages messages
+            WHERE topic.visible = :visible
+            AND topic.status IN (:arrayStatus)
+            ORDER BY topic.position ASC, topic.updated DESC'
+        )->setParameter('visible', true)
+            ->setParameter('arrayStatus', $arrayStatus)
+            ->setMaxResults(40);
+
+        $topicCollection = $query->getResult();
+
+        return $this->render('AcmeEsBattleBundle:Home:index.html.twig',
+            array('aTopic'=>$topicCollection)
+        );
+    }
+
+    public function newsAction(){
         $response = new Response();
         return $response;
     }
